6d3059ac49e500d5a409d1703a0b6d86ab3b60f0
zurfyx <zurfyx@gmail.com>
2016-12-08 01:10:55 +0100

    added references

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# travis-build
+# Automatic deploy to GitHub Pages with Travis
 
 ![](https://api.travis-ci.org/zurfyx/travis-build.svg?branch=master)
+
+### Links of interest
+
+- [Automatically Update GitHub pages with Travis example](http://www.steveklabnik.com/automatically_update_github_pages_with_travis_example/)
+- [Publishing GH-Pages with Travis CI](https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db#.1pbwnjand)
+- [Exploring the TravisCI configuration file](http://blog.tgrrtt.com/exploring-the-travisci-configuration-file)
+- [Travis won't fail on after_*](https://github.com/travis-ci/travis-ci/issues/758#issuecomment-10325537)
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './travis_logo.png';
 import './App.css';
 
 class App extends Component {
@@ -8,14 +8,19 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Travis</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <ul>
+            <li><a href="http://www.steveklabnik.com/automatically_update_github_pages_with_travis_example/">Automatically Update GitHub pages with Travis example</a></li>
+            <li><a href="https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db#.1pbwnjand">Publishing GH-Pages with Travis CI</a></li>
+            <li><a href="http://blog.tgrrtt.com/exploring-the-travisci-configuration-file">Exploring the TravisCI configuration file</a></li>
+            <li><a href="https://github.com/travis-ci/travis-ci/issues/758#issuecomment-10325537">Travis won't fail on after_*</a></li>
+          </ul>
         </p>
       </div>
     );
   }
 }
 
-export default;
+export default App;
